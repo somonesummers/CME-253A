22dd33c3bdc04cb9adfd06d0062d706518bffa07
Paul Summers <psummers@stanford.edu>
2019-06-27 11:20:40 -0700

    changing notation on iy for easier movmenet to CUDA

--- session 1/wave_2D_l_v.m
+++ session 1/wave_2D_l_v.m
@@ -14,7 +14,7 @@ dx  = Lx/nx;
 dy  = Ly/ny;
 T = 3;
 dt  = min([dx,dy])^2/mu/4.1/3/4; 
-nt = round(T/dt)
+nt = round(T/dt);
 plot_step = round(.1/dt);
 % Initial arrays
 x    = zeros((nx  )*(ny  ),1);
@@ -31,56 +31,56 @@ Vx   = zeros((nx+1)*(ny  ),1);
 Vy   = zeros((nx  )*(ny+1),1);
 % Initial conditions
 for ix = 1:nx+1
-    for iy = 1:ny+1
-        if(iy <= ny && ix <= nx)
-            x(ix+(iy-1)*nx) = (ix-1)*dx + (-Lx+dx)/2;
-            y(ix+(iy-1)*nx) = (iy-1)*dy + (-Ly+dy)/2;
-            P(ix+(iy-1)*nx) = exp(-(x(ix+(iy-1)*nx)^2+y(ix+(iy-1)*nx)^2));
-            Pini(ix+(iy-1)*nx)=P(ix+(iy-1)*nx);
+    for iyM = 1:ny+1, iy = iyM-1;
+        if(iyM <= ny && ix <= nx)
+            x(ix+(iy)*nx) = (ix-1)*dx + (-Lx+dx)/2;
+            y(ix+(iy)*nx) = (iy)*dy + (-Ly+dy)/2;
+            P(ix+(iy)*nx) = exp(-(x(ix+(iy)*nx)^2+y(ix+(iy)*nx)^2));
+            Pini(ix+(iy)*nx)=P(ix+(iy)*nx);
         end
-        xe(ix+(iy-1)*(nx+1)) = (ix-1)*dx + (-Lx)/2;
-        ye(ix+(iy-1)*(nx+1)) = (iy-1)*dy + (-Ly)/2;
+        xe(ix+(iy)*(nx+1)) = (ix-1)*dx + (-Lx)/2;
+        ye(ix+(iy)*(nx+1)) = (iy)*dy + (-Ly)/2;
     end
 end
 % Action
 for it = 1:nt
     %Velocity Updates
     for ix = 2:nx
-        for iy = 1:ny
-            Vx(ix+(iy-1)*(nx+1)) = Vx(ix+(iy-1)*(nx+1)) + dt/rho*(...
-                -1*(P(ix+(iy-1)*nx)-P((ix-1)+(iy-1)*nx))/dx...             %dP/dx
-                + (Txx(ix+(iy-1)*nx) - Txx((ix-1)+(iy-1)*nx))/dx...        %dTxx/dx
-                + (Txy(ix+(iy)*(nx+1)) - Txy(ix+(iy-1)*(nx+1)))/dy);       %dTxy/dy
+        for iyM = 1:ny, iy = iyM-1;
+            Vx(ix+(iy)*(nx+1)) = Vx(ix+(iy)*(nx+1)) + dt/rho*(...
+                -1*(P(ix+(iy)*nx)-P((ix-1)+(iy)*nx))/dx...             %dP/dx
+                + (Txx(ix+(iy)*nx) - Txx((ix-1)+(iy)*nx))/dx...        %dTxx/dx
+                + (Txy(ix+(iy+1)*(nx+1)) - Txy(ix+(iy)*(nx+1)))/dy);       %dTxy/dy
         end
     end
     for ix = 1:nx
-        for iy = 2:ny
-            Vy(ix+(iy-1)*(nx)) = Vy(ix+(iy-1)*(nx)) + dt/rho*(...
-                -1*(P(ix+(iy-1)*nx)-P(ix+(iy-2)*nx))/dy...                 %dP/dy
-                + (Tyy(ix+(iy-1)*nx) - Tyy(ix+(iy-2)*nx))/dy...            %dTyy/dy
-                + (Txy((ix+1)+(iy-1)*(nx+1)) - Txy(ix+(iy-1)*(nx+1)))/dx); %dTxy/dx
+        for iyM = 2:ny, iy = iyM-1;
+            Vy(ix+(iy)*(nx)) = Vy(ix+(iy)*(nx)) + dt/rho*(...
+                -1*(P(ix+(iy)*nx)-P(ix+(iy-1)*nx))/dy...                 %dP/dy
+                + (Tyy(ix+(iy)*nx) - Tyy(ix+(iy-1)*nx))/dy...            %dTyy/dy
+                + (Txy((ix+1)+(iy)*(nx+1)) - Txy(ix+(iy)*(nx+1)))/dx); %dTxy/dx
         end
     end
     %Pressue/Txx/Tyy Updates
     for ix = 1:nx
-        for iy = 1:ny
-            div = (Vx((ix+1)+(iy-1)*(nx+1))-Vx(ix+(iy-1)*(nx+1)))/dx+...
-                  (Vy(ix+(iy)*(nx))-Vy(ix+(iy-1)*(nx)))/dy;
-            P(ix+(iy-1)*nx) = P(ix+(iy-1)*nx) - dt*k*(div);
-            Txx(ix+(iy-1)*nx) = 2*mu*(...
-                    (Vx((ix+1)+(iy-1)*(nx+1))-Vx(ix+(iy-1)*(nx+1)))/dx - ...
+        for iyM = 1:ny, iy = iyM-1;
+            div = (Vx((ix+1)+(iy)*(nx+1))-Vx(ix+(iy)*(nx+1)))/dx+...
+                  (Vy(ix+(iy+1)*(nx))-Vy(ix+(iy)*(nx)))/dy;
+            P(ix+(iy)*nx) = P(ix+(iy)*nx) - dt*k*(div);
+            Txx(ix+(iy)*nx) = 2*mu*(...
+                    (Vx((ix+1)+(iy)*(nx+1))-Vx(ix+(iy)*(nx+1)))/dx - ...
                     1/3*(div));
-            Tyy(ix+(iy-1)*nx) = 2*mu*(...
-                    (Vy(ix+(iy)*(nx))-Vy(ix+(iy-1)*(nx)))/dy - ...
+            Tyy(ix+(iy)*nx) = 2*mu*(...
+                    (Vy(ix+(iy+1)*(nx))-Vy(ix+(iy)*(nx)))/dy - ...
                     1/3*(div));
         end
     end
     %Txy Updates (shear stress at all bondaries = 0)
     for ix = 2:nx
-        for iy = 2:ny
-            Txy(ix+(iy-1)*(nx+1)) = mu*(...
-                   (Vx(ix+(iy-1)*(nx+1)) - Vx(ix+(iy-2)*(nx+1)))/dy + ...
-                   (Vy(ix+(iy-1)*(nx)) - Vy((ix-1)+(iy-1)*(nx)))/dx);
+        for iyM = 2:ny, iy = iyM-1;
+            Txy(ix+(iy)*(nx+1)) = mu*(...
+                   (Vx(ix+(iy)*(nx+1)) - Vx(ix+(iy-1)*(nx+1)))/dy + ...
+                   (Vy(ix+(iy)*(nx)) - Vy((ix-1)+(iy)*(nx)))/dx);
         end
     end
     ttime = round(dt*it,2);
